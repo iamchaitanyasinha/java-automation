ee2cbb739aa5a5db6bef6c659ab4af6dd4f6f099
iamchaitanyasinha <er.chaitanya.sinha@gmail.com>
2024-09-28 13:05:26 +0530

    Modified the class for practice
    Modified the class for practice

--- JavaPractice1/src/Strings/StringsPractice.java
+++ JavaPractice1/src/Strings/StringsPractice.java
@@ -1,5 +1,11 @@
 package Strings;
 
+import java.util.ArrayList;
+
 public class StringsPractice {
     //This is a sting practice class
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
 }
